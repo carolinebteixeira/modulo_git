9b4cd17df8d9604e9432c125c21f4966b1bcb4af
Carol <caroline.teixeira123@gmail.com>
2025-01-09 09:23:56 -0300

    Adiciona exercícios TypeScript: multiplicação e saudação

---
+++ src/exercicio.js
@@ -0,0 +1,9 @@
+"use strict";
+function multiplicar(a, b) {
+    return a * b;
+}
+function saudacao(nome) {
+    return `Olá ${nome}`;
+}
+console.log(multiplicar(3, 4));
+console.log(saudacao("João"));
